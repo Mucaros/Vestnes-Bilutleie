8f8fd2c08c8ec78fd07ab11568d83111c48dac32
Mucaro <sillymucaro@gmail.com>
2026-01-19 19:28:49 +0100

    Improve homepage layout and add parallax background
    Refactored HomePage and related CSS for better layout and responsiveness. Added a parallax background image to the homepage, updated container and image styles for improved alignment and scaling, and introduced a responsive font size for smaller screens.

--- vestnes-nettside/src/Pages/HomePage/HomePage.jsx
+++ vestnes-nettside/src/Pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import './HomePage.css'
 import Button from '../../Components/Button.jsx'
 import ParagraphImg from '../../Components/Paragraph+img.jsx'
+import Tesla from '../../assets/Tesla-model-y-bg.avif'
 
 export default function HomePage(){
     return(
         <>
         <div className='background-container'>
             <div className='header-main-text-container'>
+                <img className='parallax-effect-img' src={Tesla} alt="does not work"/>
                 <h1 className='img-header-title'>Trenger du <br/> bil til oppkjøring?</h1>
                 <p className='header-support-text'>vi kan hjelpe med "det og det"</p>
                 <Button placeholder='Ta Kontakt'/>
@@ -23,7 +25,7 @@ export default function HomePage(){
             <ParagraphImg 
             header='Big header' 
             text='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' 
-            img='./../assets/Tesla-model-y-bg.avif'/>
+            img={Tesla}/>
         </div>
        
         </>
