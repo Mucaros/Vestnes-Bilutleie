439ff622ffb34e8c37c63ae545f664effa1b33ca
Mucaro <sillymucaro@gmail.com>
2026-01-19 13:43:01 +0100

    Add Button component and update HomePage layout
    Introduced a reusable Button component with styling. Updated HomePage to use the new Button and improved its layout and styles, including a background image and a list of locations. Added global and HomePage-specific CSS for consistent design. Also added react-router-dom as a dependency.

--- vestnes-nettside/src/Pages/HomePage/HomePage.jsx
+++ vestnes-nettside/src/Pages/HomePage/HomePage.jsx
@@ -1,5 +1,24 @@
-import 'HomePage.css'
+import './HomePage.css'
+import Button from '../../Components/Button.jsx'
 
 export default function HomePage(){
-
+    return(
+        <>
+        <div className='background-container'>
+            <div className='header-main-text-container'>
+                <h1 className='img-header-title'>Trenger du <br/> bil til oppkjøring?</h1>
+                <p className='header-support-text'>vi kan hjelpe med "det og det"</p>
+                <Button placeholder='Ta Kontakt'/>
+            </div>
+            <ul className='places-list'>
+                <li>Åsane</li>
+                <li>Bergen</li>
+                <li>Voss</li>
+                <li>Stavanger</li>
+                <li>Stord</li>
+            </ul>
+        </div>
+       
+        </>
+    )
 }
