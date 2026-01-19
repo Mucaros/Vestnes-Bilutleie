f84ae67fc07693e492e196c59fd43dd3d22aeb98
Mucaro <sillymucaro@gmail.com>
2026-01-19 11:36:17 +0100

    Introductory setup of react + extras

---
+++ vestnes-nettside/src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './Pages/HomePage/HomePage'
+import './App.css'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage/>}></Route>
+    </Routes>
+  )
+}
+
+export default App
