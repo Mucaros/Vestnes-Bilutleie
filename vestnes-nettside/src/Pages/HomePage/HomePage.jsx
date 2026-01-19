ca8ef62561820a144922b446fc81882c8244dc30
Mucaro <sillymucaro@gmail.com>
2026-01-19 20:27:04 +0100

    Add location images section to HomePage
    Added Bergen, Åsane, and Stord images to the public directory and imported them into HomePage. Introduced a new section on the homepage displaying these locations with updated CSS for layout and styling. Also improved button styling for better visual consistency.

--- vestnes-nettside/src/Pages/HomePage/HomePage.jsx
+++ vestnes-nettside/src/Pages/HomePage/HomePage.jsx
@@ -2,6 +2,9 @@ import './HomePage.css'
 import Button from '../../Components/Button.jsx'
 import ParagraphImg from '../../Components/Paragraph+img.jsx'
 import Tesla from '../../assets/Tesla-model-y-bg.avif'
+import Bergen from '../../../public/Bergen.jpg'
+import Åsane from '../../../public/Åsane.jpg'
+import Stord from '../../../public/Stord.jpg'
 
 export default function HomePage(){
     return(
@@ -27,7 +30,20 @@ export default function HomePage(){
             text='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' 
             img={Tesla}/>
         </div>
-       
+       <div className='image-location-container'>
+            <div className='left-side-text'>
+                <h1 className='text-should-stay-fixed'>Du kan ta lappen på alle disse stedene</h1>
+            </div>
+            <div className='right-side-images'>
+                <img className='moving-img' src={Bergen} alt="" />
+                <img className='moving-img' src={Åsane} alt="" />
+                <img className='moving-img' src={Stord} alt="" />
+            </div>
+       </div>
+
+       <div className='TEST'>
+            
+       </div>
         </>
     )
 }
