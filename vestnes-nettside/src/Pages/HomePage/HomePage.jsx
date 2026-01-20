ecb1dabcf0ec597f5249697e002be46d6c516ba6
Mucaro <sillymucaro@gmail.com>
2026-01-20 16:08:04 +0100

    Add new homepage layout and GeneralSans font
    Introduces a redesigned HomePage using a new layout and styles in Homepage2.css, updates HomePage.jsx to use the new structure, and adds the GeneralSans font family with all weights and styles. Updates global and component CSS for improved responsiveness and appearance. Adds a new image and updates font-face declarations in index.css.

--- vestnes-nettside/src/Pages/HomePage/HomePage.jsx
+++ vestnes-nettside/src/Pages/HomePage/HomePage.jsx
@@ -1,49 +1,47 @@
-import './HomePage.css'
-import Button from '../../Components/Button.jsx'
-import ParagraphImg from '../../Components/Paragraph+img.jsx'
+import './HomePage2.css'
 import Tesla from '../../assets/Tesla-model-y-bg.avif'
+
+/*import Button from '../../Components/Button.jsx'
+import ParagraphImg from '../../Components/Paragraph+img.jsx'
 import Bergen from '../../../public/Bergen.jpg'
 import Åsane from '../../../public/Åsane.jpg'
 import Stord from '../../../public/Stord.jpg'
+import Eivind from '../../../public/Eivind-bil.jpg'*/
 
-export default function HomePage(){
-    return(
+export default function HomePage() {
+    return (
         <>
-        <div className='background-container'>
-            <div className='header-main-text-container'>
-                <img className='parallax-effect-img' src={Tesla} alt="does not work"/>
-                <h1 className='img-header-title'>Trenger du <br/> bil til oppkjøring?</h1>
-                <p className='header-support-text'>vi kan hjelpe med "det og det"</p>
-                <Button placeholder='Ta Kontakt'/>
-            </div>
-            <ul className='places-list'>
-                <li>Åsane</li>
-                <li>Bergen</li>
-                <li>Voss</li>
-                <li>Stavanger</li>
-                <li>Stord</li>
-            </ul>
-        </div>
-        <div className='paragraph-container'>
-            <ParagraphImg 
-            header='Big header' 
-            text='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' 
-            img={Tesla}/>
-        </div>
-       <div className='image-location-container'>
-            <div className='left-side-text'>
-                <h1 className='text-should-stay-fixed'>Du kan ta lappen på alle disse stedene</h1>
-            </div>
-            <div className='right-side-images'>
-                <img className='moving-img' src={Bergen} alt="" />
-                <img className='moving-img' src={Åsane} alt="" />
-                <img className='moving-img' src={Stord} alt="" />
-            </div>
-       </div>
+            <div className='scroll-watcher'></div>
 
-       <div className='TEST'>
+            <header class="primary-header">
+                <img className='hero-img' src={Tesla} alt="" />
+                <div className='hero-text-container'>
+                    <h1 className='header-title'>Leie av bil <br /> til førerprøven</h1>
+                    <p className='header-subtext'>Skal du opp til førerprøven og trenger bil?</p>
+                </div>
+            </header>
+
+            <section className='content'>
+
+                <div className='paragraph-container'>
+                    <h1 className='paragraph-title'>Eivind Vestnes</h1>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.               </p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.               </p>
+                </div>
+                <img src={Tesla} alt="" />
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.               </p>
+
+                <img src={Tesla} alt="" />
+
+                <div className='paragraph-container'>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.               </p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.               </p>
+                </div>
+            </section>
+
             
-       </div>
         </>
+
     )
 }
+
