d36ce074dd2bf7c3cb346d1167215453b1c6a14c
Mucaro <sillymucaro@gmail.com>
2026-01-22 16:04:43 +0100

    Revamp HomePage layout and styling
    Updated HomePage.jsx to improve image imports, add a new hero image, revise header text, introduce fade-in animations, restructure info cards, and add a contact form. Homepage2.css was refactored for new animations, sticky info cards, and improved layout. index.css now includes global font and body styling for better appearance. Added red-tesla.jpg as a new asset.

--- vestnes-nettside/src/Pages/HomePage/HomePage.jsx
+++ vestnes-nettside/src/Pages/HomePage/HomePage.jsx
@@ -1,9 +1,9 @@
 import './HomePage2.css'
 import Tesla from '../../assets/Tesla-model-y-bg.avif'
-
-import Bergen from '../../../public/Bergen.jpg'
+import redTesla from '/red-tesla.jpg'
+import Bergen from '/Bergen.jpg'
 import Åsane from '../../../public/Åsane.jpg'
-import Stord from '../../../public/Stord.jpg'
+import Stord from '/Stord.jpg'
 // import Eivind from '../../../public/Eivind-bil.jpg'
 
 export default function HomePage() {
@@ -12,10 +12,10 @@ export default function HomePage() {
             <div className='scroll-watcher'></div>
 
             <header class="primary-header">
-                <img className='hero-img' src={Tesla} alt="" />
+                <img className='hero-img' src={redTesla} alt="" />
                 <div className='hero-text-container'>
-                    <h1 className='header-title'>Leie av bil <br /> til førerprøven</h1>
-                    <p className='header-subtext'>Skal du opp til førerprøven og trenger bil?</p>
+                    <h1 className='header-title'>Trenger du leie av bil <br /> til førerprøven?</h1>
+                    <p className='header-subtext'>Vestnes har løsningen</p>
                 </div>
             </header>
 
@@ -26,7 +26,8 @@ export default function HomePage() {
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.               </p>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.               </p>
                 </div>
-                <img src={Tesla} alt="" />
+
+                <img className='fade-in' src={Tesla} alt="" />
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.               </p>
 
 
@@ -36,29 +37,48 @@ export default function HomePage() {
                         <h1 className='scrolling-title'>Velg hvor du ønsker å ta oppkjøring</h1>
                     </div>
                     <div className='scrolling-image-container'>
-                        <img className='scrolling-image' src={Bergen} alt="" />
-                        <img className='scrolling-image' src={Åsane} alt="" />
-                        <img className='scrolling-image' src={Stord} alt="" />
+                        <img className='scrolling-image fade-in' src={Bergen} alt="" />
+                        <img className='scrolling-image fade-in' src={Åsane} alt="" />
+                        <img className='scrolling-image fade-in' src={Stord} alt="" />
                     </div>
                 </div>
 
+                <div className='paragraph-container'>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.               </p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.               </p>
+                </div>
+
                 <div className='info-cards-wrapper'>
-                    <div className='info-card'>
+                    <div className='info-card first-card'>
+                        <h2 className='info-card-title'>Levering av bil</h2>
+                        <p className='info-card-description'>Leie av bil inkluderer levering til oppkjøring i Drammen og Billingstad uten ekstra kostnad. Dersom du ønsker oppkjøring andre steder - for eksempel Asker omegn, Hønefoss, Kongsberg eller Tønsberg - ordner vi dette mot et tillegg i prisen.</p>
+                    </div>
+                    <div className='info-card second-card'>
                         <h2 className='info-card-title'>M/oppvarmingstime</h2>
                         <p className='info-card-description'>Møt opp på vår trafikkskole 45 minutter før oppkjøringen. Du får tid til å kjøre bilen, bli vant til den, og fjerne unødvendige nerver før prøven.</p>
                     </div>
-                    <div className='info-card yellow'>
+                    <div className='info-card third-card'>
                         <h2 className='info-card-title'>U/oppvarmingstime</h2>
                         <p className='info-card-description'>Ønsker du å møte direkte på trafikkstasjonen? En av våre ansatte møter deg der ca. 10 minutter før førerprøven, slik at du kan justere sete og speil før du starter.</p>
                     </div>
                 </div>
 
-                <img src={Tesla} alt="" />
-                <div className='paragraph-container'>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.               </p>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.               </p>
+                <div className='form-container'>
+                    <form action="">
+                        <label htmlFor="fname">Navn:</label>
+                        <input type="text" id="fname" name="fname"></input>
+                        <label htmlFor="phone">Tlf:</label>
+                        <input type="number" id="phone" name="lname"></input>
+                        <label htmlFor="email">email:</label>
+                        <input type="email" id="email" name="email"/>
+                        <label htmlFor="melding">melding:</label>
+                        <textarea id="message" name="message"></textarea>
+                        <input type="submit" value="Submit"></input>
+                    </form>
                 </div>
+
             </section>
+
             <footer>
                 <ul className='footer-list'>
                     <li className='footer-list-element'>Eivind Vestnes</li>
