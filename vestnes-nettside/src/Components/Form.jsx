fc3bb8c449987e6f480ee5e0aca21f74f7813779
Mucaro <sillymucaro@gmail.com>
2026-01-25 15:57:53 +0100

    Update form to use Web3Forms and add validation
    The contact form now submits to Web3Forms with a required access key and uses 'required' attributes for all fields to enforce validation. Minor whitespace changes were made to HomePage.css.

--- vestnes-nettside/src/Components/Form.jsx
+++ vestnes-nettside/src/Components/Form.jsx
@@ -3,16 +3,17 @@ import './Form.css'
 export default function Form() {
     return (
         <div className='form-container'>
-            <form action="">
+            <form action="https://api.web3forms.com/submit" method='post'>
+                <input type="hidden" name="access_key" value="8cde912f-d6e9-4824-8b8a-664d9ad3ad00"></input>
                 <label htmlFor="fname">Navn:</label>
-                <input type="text" id="fname" name="fname"></input>
+                <input type="text" id="fname" name="fname" required></input>
                 <label htmlFor="phone">Tlf:</label>
-                <input type="number" id="phone" name="phone"></input>
+                <input type="number" id="phone" name="phone" required></input>
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" />
+                <input type="email" id="email" name="email" required/>
                 <label htmlFor="message">Melding:</label>
-                <textarea id="message" name="message"></textarea>
-                <button className='submit-button' type="submit" value="submit">Send</button>
+                <textarea id="message" name="message" required></textarea>
+                <button className='submit-button' type="submit" value="submit" required>Send</button>
             </form>
         </div>
     )
